13dc5ab76358d23b49b3a40825071d142328bdc3
Anri Gogiberidze <anri45gogiberidze@gmail.com>
2022-01-27 20:51:11 +0400

    Responsiveness + Rules and styling

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,58 @@
 function startGame() {
+    var secretNumber = Math.floor(Math.random(1, 10) * 11);
+    console.log(secretNumber);
+
     const startBtn = document.getElementById('startBtn');
     startBtn.style.display = 'none';
     const gameControls = document.getElementById('gameControls');
     gameControls.style.display = 'flex';
     const gmText = document.getElementById('gmText');
     gmText.innerText = "I have selected a number. Now guess it!"
+    const resetBtn = document.getElementById('resetBtn');
+    resetBtn.style.display = 'none';
+
+    document.getElementById('userNuberSub').onclick = function() {
+
+        let userGuess = document.getElementById('userNuberInp').value;
+    
+        if(userGuess == secretNumber) {
+            gmText.innerText = "Congratulations! You have guessed correctly";
+            gameControls.style.display = 'none';
+            resetBtn.style.display = 'block';
+        } else if(userGuess > secretNumber) {
+            gmText.innerText = "You have guessed high. Go lower.";
+        } else if(userGuess == '') {
+            gmText.innerText = "You have to input a number.";
+        } else {
+            gmText.innerText = "You have guessed low. Go higher.";
+        }
+    }
 }
 
-let secretNumber = Math.floor(Math.random(1, 10) * 11);
-console.log(secretNumber);
 
-document.getElementById('userNuberSub').onclick = function() {
-
-    let userGuess = document.getElementById('userNuberInp').value;
-
-    if(userGuess == secretNumber) {
-        gmText.innerText = "Congratulations! You have guessed correctly";
-    } else if(userGuess > secretNumber) {
-        gmText.innerText = "You have guessed high. Go lower.";
-    } else if(userGuess == '') {
-        gmText.innerText = "You have to input a number.";
-    } else {
-        gmText.innerText = "You have guessed low. Go higher.";
-    }
+function resetGame() {
+    startGame();
 }
 
 function musicControl() {
     const gameAudio = document.getElementById('gameAudio');
-    gameAudio.remove();
+    gameAudio.remove();   
 }
+
+function rotate() {
+    const rotatingCoinLeft = document.getElementById('rotatingCoinLeft');
+    if(!rotatingCoinLeft.classList.contains('rotateLeft')) {
+        rotatingCoinLeft.classList.add('rotateLeft');
+    } else {
+        rotatingCoinLeft.classList.remove('rotateLeft');
+    }
+
+    const rotatingCoinRight = document.getElementById('rotatingCoinRight');
+    if(!rotatingCoinRight.classList.contains('rotateLeft')) {
+        rotatingCoinRight.classList.add('rotateLeft');
+    } else {
+        rotatingCoinRight.classList.remove('rotateLeft');
+    }
+}
+
+let rotateInterval = setInterval(rotate, 500);
